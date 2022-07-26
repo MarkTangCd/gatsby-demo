f9109837c1239de008d60041f02d1c1ed0972122
Mark Tang <MarkTangCd@hotmail.com>
2022-07-26 14:58:08 +0800

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-postcss", {
+  plugins: ["gatsby-plugin-emotion", {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
       "trackingId": "dafdsfdssdsfdsfddfdsaf"
